4791d3f614f75972df401ee7e0e130f3b612e0e2
Martin Espericueta <martin.m.espericueta@gmail.com>
2026-01-08 09:39:14 -0800

    chore: configure styled components

---
+++ src/providers/StyledComponentsRegistry.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { useState } from 'react'
+import { StyleSheetManager } from 'styled-components'
+import { useServerInsertedHTML } from 'next/navigation'
+
+export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
+  const [styledComponentsStyleSheet] = useState(
+    () => new (require('styled-components').ServerStyleSheet)()
+  )
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement()
+    styledComponentsStyleSheet.instance.clearTag()
+    return <>{styles}</>
+  })
+
+  return (
+    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+      {children}
+    </StyleSheetManager>
+  )
+}
