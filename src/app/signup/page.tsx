f609ec94c6f8455061661f6fbc1ee1264dfe8fe7
Martin Espericueta <martin.m.espericueta@gmail.com>
2026-01-10 01:22:36 -0800

    fix(pages): replace H1 with Text component

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,9 @@
+import Text from '@GlamUI/components/Text'
+
 const Signup = () => {
   return (
     <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16  sm:items-start">
-      <h1>Registrate!</h1>
+      <Text as="h1">Registrate!</Text>
     </main>
   );
 }
