fb7aace0a6ca4c9cd5d04881c711fa4b3d422eff
Martin Espericueta <martin.m.espericueta@gmail.com>
2026-01-12 11:05:38 -0800

    feat(tokens): configurar compilado de tokens

---
+++ src/GlamUI/tokens/dist/colors.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.colors = void 0;
+exports.colors = {
+    brand: {
+        primary: '#EA638C',
+        secondary: '#F6339A',
+        primaryAlpha: 'rgba(234, 99, 140, 0.25)',
+    },
+    neutral: {
+        900: '#111827',
+        600: '#4b5563',
+        400: '#9ca3af',
+    },
+    feedback: {
+        errorBg: '#fde2e2',
+        errorText: '#991b1b',
+        successBg: '#dcfce7',
+        successText: '#166534',
+        warningBg: '#fef3c7',
+        warningText: '#92400e',
+        infoBg: '#e0f2fe',
+        infoText: '#075985',
+    },
+    danger: '#ef4444',
+    white: '#ffffff',
+};
---
+++ src/GlamUI/tokens/dist/typography.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.typography = void 0;
+exports.typography = {
+    sizes: {
+        xs: '12px',
+        sm: '14px',
+        md: '16px',
+        lg: '18px',
+        xl: '24px',
+        xxl: '32px',
+    },
+    lineHeights: {
+        tight: '1.2',
+        normal: '1.5',
+        relaxed: '1.7',
+    },
+    weights: {
+        regular: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+    },
+};
