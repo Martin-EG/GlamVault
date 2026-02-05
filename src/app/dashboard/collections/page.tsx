8c769e6d8496085687d331dce2c81dc9d2a18a7e
Martin Espericueta <martin.m.espericueta@gmail.com>
2026-02-05 10:58:58 -0800

    test(lint): Add description in collections page with span

--- src/app/dashboard/collections/page.tsx
+++ src/app/dashboard/collections/page.tsx
@@ -6,7 +6,8 @@ import Text from '@/GlamUI/components/Text'
 const Collections = () => {
   return (
     <main className="flex h-full w-full max-w-3xl flex-col items-center justify-between py-32 px-16  sm:items-start">
-      <Text as="h1">Collections</Text>
+      <Text as="h20">Collections</Text>
+      <span>This is the page where user will see their collections</span>
     </main>
   );
 }
