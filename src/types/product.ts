ed88d3ce5e444b0b3317fbb8cb4b3a394899335f
Martin Espericueta <martin.m.espericueta@gmail.com>
2026-02-10 09:14:24 -0800

    chore(types): move product type to types folder

---
+++ src/types/product.ts
@@ -0,0 +1,11 @@
+export type Product = {
+  id: string;
+  name: string;
+  makeupType: string;
+  brand: string;
+  openedAt: Date;
+  boughtIn: string;
+  description: string;
+  collectionId: string;
+  image: string;
+};
