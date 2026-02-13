f4d2a6de1a92dfa08307c5bcead5c2aad6c0cff2
Martin Espericueta <martin.m.espericueta@gmail.com>
2026-02-13 15:57:06 -0800

    refactor(strings): split strings into a one file for each section and add consistency test

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -23,9 +23,27 @@ async function getLocaleFromHeader(): Promise<Locale> {
 
 export default getRequestConfig(async () => {
   const locale = await getLocaleFromHeader();
+  const messages = {
+    common: (await import(`../../messages/${locale}/common.json`)).default,
+    dashboard: (await import(`../../messages/${locale}/dashboard.json`))
+      .default,
+    errors: (await import(`../../messages/${locale}/errors.json`)).default,
+    inventory: (await import(`../../messages/${locale}/inventory.json`))
+      .default,
+    inventoryAddProduct: (
+      await import(`../../messages/${locale}/inventoryAddProduct.json`)
+    ).default,
+    login: (await import(`../../messages/${locale}/login.json`)).default,
+    navigation: (await import(`../../messages/${locale}/navigation.json`))
+      .default,
+    passwordRecovery: (
+      await import(`../../messages/${locale}/passwordRecovery.json`)
+    ).default,
+    signup: (await import(`../../messages/${locale}/signup.json`)).default,
+  };
 
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    messages,
   };
 });
